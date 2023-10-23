69043a3292a70481a737597adeab18e162c2ef2c
Angeliko Centeno <angeliko.digiteeracademy@gmail.com>
2023-10-23 11:52:31 +0800

    updated links and padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,10 @@ import Facebook from '../components/img/facebook.svg'
 import Twitter from '../components/img/twitter.svg'
 import Instagram from '../components/img/instagram.svg'
 import LinkedIn from '../components/img/linkedin.svg'
-import Email from '../components/img/email.svg'
 import Contact from '../components/img/contact.svg'
 import Address from '../components/img/address.svg'
 import Stack from 'react-bootstrap/Stack';
-
+import Row from 'react-bootstrap/Row';
 
 
 
@@ -17,12 +16,13 @@ const Footer = () => {
     return (
         <section className='Footer fluid py-20'>
             <Container>
+                <Row className='text-white text-center'>
+                    <h2>Angeliko Centeno</h2>
+                    <h1>angeliko.digiteeracademy@gmail.com</h1>
+                    <h3>Golden City, Binan City, Laguna</h3>
+                </Row>
                 <Stack direction="horizontal" gap={3}>
                     <Stack gap={3}>
-                        <div className="p-2 text-white">
-                            <img src={Email} alt="email" className='img-fluid' />
-                            angeliko.digiteeracademy@gmail.com
-                        </div>
                         <div className="p-2 text-white">
                             <img src={Contact} alt="contact" className='img-fluid' />
                             +63 9203487694
@@ -32,16 +32,16 @@ const Footer = () => {
                             Binan, Laguna
                         </div>
                     </Stack>
-                    <Link to="/" className="ms-auto">
+                    <Link target='blank' to="https://www.facebook.com/aintgeliko" className="ms-auto">
                         <img src={Facebook} alt="facebook" className='img-fluid' />
                     </Link>
-                    <Link to="/" >
+                    <Link target='blank' to="https://twitter.com/aintgeliko">
                         <img src={Twitter} alt="twitter" className='img-fluid' />
                     </Link>
-                    <Link to="/">
+                    <Link target='blank' to="https://www.instagram.com/aintgeliko/">
                         <img src={Instagram} alt="instagram" className='img-fluid' />
                     </Link>
-                    <Link to="/">
+                    <Link target='blank' to="https://www.linkedin.com/in/angeliko-roland-j-centeno-77857a267/">
                         <img src={LinkedIn} alt="linkedin" className='img-fluid' />
                     </Link>
                 </Stack>
